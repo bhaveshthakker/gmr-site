e065e6b618ca9eab7cbd5cefe80e0fdc00962bee
malav-shah <malav42@gmail.com>
2014-04-24 00:10:10 +0530

    preferred companies enhancement

--- updateprofile.php
+++ updateprofile.php
@@ -12,10 +12,12 @@
 		<img src="img/ajax-loader.gif" id="loading-img" style="display:none;" alt="Please Wait"/>
 		<div id="progressbox" ><div id="progressbar"></div ><div id="statustxt">0%</div></div>
 		<div id="output"></div>
+		
 		<textarea rows="3" placeholder="List the name of your preferred companies"></textarea>
-		<select id="chosen-plugin" data-placeholder="Choose a Country..." class="chosen-select" multiple style="width:350px;" tabindex="4">
+		<select id="chosen-plugin" data-placeholder="Choose your preferred companies..." class="chosen-select" multiple style="width:100%;" tabindex="4">
             <option value=""></option>
         </select>
+		
 		<textarea rows="3" placeholder="List the name of your preferred locations"></textarea>
 		<input type="submit" value="Update" class="cform-submit">
 
