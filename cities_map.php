30be70feaf8fb1e847e7151697e9b4b54337c651
Bhavesh <bhaveshthakker111@gmail.com>
2014-05-04 17:52:26 +0530

    completing the flow of company details and cities details

---
+++ cities_map.php
@@ -0,0 +1,24 @@
+<?php
+require_once('session_initialize.php'); 
+include_once 'database.php';
+
+if(isset($_SESSION['username'])) {
+	$query = "select id, name from location_details";
+	//echo $query."<br />";
+	$result = mysql_query($query) or die(mysql_error());
+	$info=array();
+	if ($result) {
+      while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        // do something with the $row
+        //echo $row[0]."-	".$row[1]."<br />";	
+        //$info = $row;
+        array_push($info, $row);
+      }
+      echo json_encode($info);
+    }
+    else {
+      die(mysql_error());
+    }
+}
+
+?>
