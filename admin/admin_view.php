2ce625956421ccfb699a24c38744775965d52d1b
Bhavesh <bhaveshthakker111@gmail.com>
2014-05-16 16:21:10 +0530

    changing admin view a bit

--- admin/admin_view.php
+++ admin/admin_view.php
@@ -10,7 +10,8 @@
 
 ?>
 	<body>
-		<table id='applicants_view' >
+		<div class="admin-page-container">
+		<table id='applicants_view' class="display" cellspacing="0" width="100%">
 		<thead>
 			<tr>
 				<th>User Name</th>
@@ -23,6 +24,8 @@
 				<th>Mobile</th>
 				<th>Current City</th>
 				<th>Resume Path</th>
+				<th>Creation Date</th>
+				<th>Status Type</th>
 			</tr>
 		</thead>
 <?php
@@ -40,6 +43,9 @@
 		  echo "<td>" . $row['mobile'] . "</td>";
 		  echo "<td>" . $row['current_city'] . "</td>";
 		  echo "<td>" . $row['resume_path'] . "</td>";
+		  echo "<td>" . $row['creation_date'] . "</td>";
+		  echo "<td>" . $row['status_type'] . "</td>";
+		  
 		  echo "</tr>";
 	  }
 	  
@@ -48,13 +54,21 @@
 
 	echo "</table>";
 ?>
+</div>
 </body>
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"></link>
 	<script type="text/javascript" src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-	<link href="css/jquery.dataTables.min.css" type="text/css" rel="stylesheet"></link>
+	<link href="css/jquery.dataTables_themeroller.min.css" type="text/css" rel="stylesheet"></link>
+	
+	<!-- <link rel="stlesheet" type="text/css" href="//cdn.datatables.net/plug-ins/28e7751dbec/integration/jqueryui/dataTables.jqueryui.css"></link> -->
 	<!--<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>-->
 	<script type="text/javascript" src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
-	    $('#applicants_view').dataTable();
+	    $('#applicants_view').dataTable({
+        "order": [[ 11, "desc" ]],
+        "pagingType": "full_numbers"
+    } );
 	});
 </script>
